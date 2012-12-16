0af1d64ada753b5565cf8af6d1593753068deeb6
Jānis Pauls <janis.pauls@gmail.com>
2012-12-16 23:24:37 +0200

    interval option added, prints response

--- lib/ic_reader/title_reader.rb
+++ lib/ic_reader/title_reader.rb
@@ -3,11 +3,14 @@ module ICReader
   class TitleReader
     def initialize args
       @uri = URI.parse args[:url]
+      @interval = args[:interval] || 5
     end
+
     def run
       loop do
-        puts "running"
-        sleep 5
+        response = @uri.read
+        p response
+        sleep @interval
       end
     end
   end
