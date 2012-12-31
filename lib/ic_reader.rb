15ab86698b3d500f878922570b4acb43fc1c5301
Jānis Pauls <janis.pauls@gmail.com>
2012-12-31 15:36:09 +0200

    added require for redis_notifer

--- lib/ic_reader.rb
+++ lib/ic_reader.rb
@@ -4,6 +4,7 @@ require "ic_reader/title_reader"
 require "ic_reader/title_parser"
 require "ic_reader/base_notifier"
 require "ic_reader/growl_notifier"
+require "ic_reader/redis_notifier"
 require "ic_reader/redis_pubsub_notifier"
 
 module ICReader
